35c6cefb6c80677e2617e2796d5a635141146fc9
Mariya <m_relaxeena@mail.ru>
2022-11-28 18:19:53 +0400

    task21

---
+++ Homework3/task21/Program.cs
@@ -0,0 +1,22 @@
+﻿int getNumberFromUser( string messege );
+{
+    int result = 0;
+    Console.WriteLine(messege);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double getDistance (double xa, double ya, double za, double xb, double yb, double zb);
+{
+    double distance = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya) , 2) + Math.Pow((zb - za), 2));
+    return distance;
+}
+int xa = getNumberFromUser("Введите X1:");
+int ya = getNumberFromUser("Введите Y1:");
+int za = getNumberFromUser("Введите Z1:");
+int xb = getNumberFromUser("Введите X2:");
+int yb = getNumberFromUser("Введите Y2:");
+int zb = getNumberFromUser("Введите Z2:");
+
+double distanse = GetDistance( xa, ya, za, xb, yb, zb);
+Console.WriteLine($"Расстояние между точками = {distance} ");
