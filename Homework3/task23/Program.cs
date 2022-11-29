a498b5991e5a640cf8ff718982a1076dadf8b958
Mariya <m_relaxeena@mail.ru>
2022-11-29 18:23:38 +0400

    task23

---
+++ Homework3/task23/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] GetCubeTable(int N)
+{
+    int[] cub = new int[N + 1];
+    for (int i = 1; i < cub.Length; i++)
+    {
+        cub[i] = i * i * i;
+    }
+    return cub;
+}
+void PrintCubs(int [] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($"{i} ^ 3 = ");
+        Console.WriteLine(array[i]);
+    }
+}
+int[] CubeTable = GetCubeTable(N);
+PrintCubs(CubeTable);
